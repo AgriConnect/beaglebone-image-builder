005256b6deb1a35985966dd7f4363e58b713b62e
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2018-02-25 16:08:46 +0700

    Add script to build BeagleBone firmware for AgriConnect use.

---
+++ build-image-for-agriconnect.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+TODAY=$(date +%Y-%m-%d)
+VERNUM=9.3
+
+generate_img() {
+	cd deploy/debian-${VERNUM}-console-armhf-${TODAY}
+	FILENAME=BBB-eMMC-flasher-debian-${VERNUM}-${TODAY}
+	sudo ./setup_sdcard.sh --img-4gb ${FILENAME} --dtb beaglebone --bbb-flasher --bbb-old-bootloader-in-emmc --hostname beaglebone
+	mv ${FILENAME} ../
+	cd ../..
+}
+
+./RootStock-NG.sh -c agriconnect_console_debian_stretch_armhf.conf
+
+
