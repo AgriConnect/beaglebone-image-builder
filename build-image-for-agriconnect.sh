03148fbcf3a8589378f7b61a6c6ff0032f801395
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2018-03-07 15:25:09 +0700

    Add additional apt & pip repo

--- build-image-for-agriconnect.sh
+++ build-image-for-agriconnect.sh
@@ -3,14 +3,50 @@
 TODAY=$(date +%Y-%m-%d)
 VERNUM=9.3
 
+check_tools() {
+	unset need_tool
+	export OLD_PATH=$PATH
+	export PATH=$PATH:/sbin:/usr/sbin
+	command -v mkfs.vfat >/dev/null 2>&1 || { echo "Missing mkfs.vfat "; need_tool=1; }
+	command -v git >/dev/null 2>&1 || { echo "Missing git "; need_tool=1; }
+	command -v partprobe >/dev/null 2>&1 || { echo "Missing partprobe "; need_tool=1; }
+	command -v kpartx >/dev/null 2>&1 || { echo "Missing kpartx "; need_tool=1; }
+	command -v bmaptool >/dev/null 2>&1 || { echo "Missing bmaptool "; need_tool=1; }
+	command -v pixz >/dev/null 2>&1 || { echo "Missing pixz "; need_tool=1; }
+	export PATH=$OLD_PATH
+	unset OLD_PATH
+
+	if [ ${need_tool} ] ; then
+		echo "need_tool: ${need_tool}"
+		echo "Please install these packages: dosfstools git-core kpartx wget parted bmap-tools pixz"
+		echo ""
+		exit
+	fi
+}
+
 generate_img() {
 	cd deploy/debian-${VERNUM}-console-armhf-${TODAY}
 	FILENAME=BBB-eMMC-flasher-debian-${VERNUM}-${TODAY}
+	echo "Setup image for SD card"
 	sudo ./setup_sdcard.sh --img-4gb ${FILENAME} --dtb beaglebone --bbb-flasher --bbb-old-bootloader-in-emmc --hostname beaglebone
 	IMGFILE=${FILENAME}-4gb.img
+	# Move to deploy/ folder
 	mv ${IMGFILE} ../
-	cd ../..
+	# Go to deploy/ folder
+	cd ..
+	# Generate bmap
+	echo "Generate bmap for ${IMGFILE}"
+	bmaptool create ${IMGFILE} -o ${FILENAME}-4gb.bmap
+	# Compress the image file
+	echo "Compress the image file ${IMGFILE}"
+	pixz ${IMGFILE}
+	# Go back to original working folder
+	cd ..
 }
+
+check_tools
+
+sudo echo "Start sudo session"
 
 ./RootStock-NG.sh -c agriconnect_console_debian_stretch_armhf.conf
 
